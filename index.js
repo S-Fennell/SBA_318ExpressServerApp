7cea941ac7088246640c2e3850323453cec68365
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-06-11 17:04:45 -0400

    figured out how to post by adding body parser where it kept coming up undefined prior

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import user from './routes/user.js';
+import bodyParser from 'body-parser'
 
 const app = express();
+app.use(bodyParser.json());
 const port = 3000;
 
 app.use('/users', user)
+
 app.get('/', (req, res) =>{
     console.log('Hola Brooklyn!')
+
     res.send(`Hola Brooklyn!`)
 })
 
--- routes/user.js
+++ routes/user.js
@@ -3,10 +3,33 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+    {
+        firstName: "Captain",
+        lastName: "America",
+        age: 80
+    }
+    
+]
+
+
 router.get('/', (req, res)=>{
     console.log(`Hello Halrem!`)
-    res.send('Hello Harlem!')
-})
+    //res.send('Hello Harlem!');
+    console.log(users);
+    
+    res.send(users);
+    
+});
+
+router.post('/', (req, res)=>{
+    console.log(`West-Side`);
+    
+    console.log(req.body);
+    //users.push()
+
+    res.send('WEst-Side');
+});
 
 
 export default router;
