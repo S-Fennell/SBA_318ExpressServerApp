1c9e532b34cbcc314ab3daaab0f199e87cbd948a
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-06-14 19:51:23 -0400

    created a view folder and turned my html file to an ejs file so I can render information from html/ejs file to user

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 import express from 'express';
-import user from './routes/user.js';
-import bodyParser from 'body-parser'
-
+import user from './routes/user.js';//connecting/importing my router from user.js
+import bodyParser from 'body-parser';
 const app = express();
-app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');// so I can render things to the user
+app.use(bodyParser.json());// this is so I can se my json file
 const port = 3000;
 
-app.use('/users', user)
+app.use('/users', user);//connecting my user router
+
 
 app.get('/', (req, res) =>{
-    console.log('Hola Brooklyn!')
+    console.log('Hola Brooklyn!');
+    res.render('index', { text: 'We have the best pizza!'});
+})
 
-    res.send(`Hola Brooklyn!`)
+app.get('/users', (req, res) =>{
+    console.log('Hola Brooklyn!');
+    res.status(200).send(`Hola Brooklyn!`);
+})
+
+app.get('/users/', (req, res) =>{
+    console.log('Hola Brooklyn!');
+    res.status(200).send(`Hola Brooklyn!`);
 })
 
 app.listen(port, ()=>{
--- routes/user.js
+++ routes/user.js
@@ -3,18 +3,16 @@ import express from 'express';
 
 const router = express.Router();
 
-const users = [
+const users = [//thisis jsut a test array
     {
         firstName: "Captain",
         lastName: "America",
         age: 80
-    }
-    
+    }   
 ]
 
-
 router.get('/', (req, res)=>{
-    console.log(`Hello Halrem!`)
+    console.log(`Hello Halrem!`);
     //res.send('Hello Harlem!');
     console.log(users);
     
@@ -25,11 +23,20 @@ router.get('/', (req, res)=>{
 router.post('/', (req, res)=>{
     console.log(`West-Side`);
     
-    console.log(req.body);
-    //users.push()
+    //console.log(req.body);
+    const user = req.body//This is where my post is being stored. I can push my post into the array below
+    users.push(user);
 
-    res.send('WEst-Side');
+    res.send(`Hey ${user.firstName}${user.lastName}! Where is the next fight for freedom?`);
 });
 
+router.get('/:id', (req, res)=>{
+    console.log(req.params);
+    const id = req.params;
 
-export default router;
+    
+    res.send(req.params)
+})
+
+
+export default router;//exporting router for index.js
