990eddedbe7521491d1741828b610a451b194634
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-06-15 15:29:06 -0400

    trying to start over step by step and figure out what's wrong

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 import express from 'express';
-import user from './routes/user.js';//connecting/importing my router from user.js
-import bodyParser from 'body-parser';
+//import user from './routes/user.js';//connecting/importing my router from user.js
+//import bodyParser from 'body-parser';
 const app = express();
 
-app.set('view engine', 'ejs');// so I can render things to the user
-app.use(bodyParser.json());// this is so I can read my json
+//app.set('view engine', 'ejs');// so I can render things to the user
+//app.use(bodyParser.json());// this is so I can read my json
 const port = 3000;
 
-app.use('/users', user);//connecting my user router
-app.use(express.static('public'))
+//app.use('/users', user);//connecting my user router
+// app.use(express.static('public'))
 
 app.get('/', (req, res) =>{
     console.log('Hola Brooklyn!');
-    res.render('index', { text: 'We have the best pizza!'});
-})
-
-
-
-app.get('/users', (req, res) =>{
-    console.log('Hola Brooklyn!');
     res.status(200).send(`Hola Brooklyn!`);
+    //res.render('index', { text: 'We have the best pizza!'});
 })
+
+
+
+// app.get('/users', (req, res) =>{
+//     console.log('Hola Brooklyn!');
+//     res.status(200).send(`Hola Brooklyn!`);
+// })
 
 
 app.listen(port, ()=>{
--- routes/user.js
+++ routes/user.js
@@ -1,46 +1,46 @@
 //This file for organizing my routes
-import express from 'express';
-const router = express.Router();
+// import express from 'express';
+// const router = express.Router();
 
-const users = [//this is jsut a test array
-    {
-        firstName: "Captain",
-        lastName: "America",
-        age: 80
-    }   
-]
+// const users = [//this is jsut a test array
+//     {
+//         firstName: "Captain",
+//         lastName: "America",
+//         age: 80
+//     }   
+// ]
 
-router.get('/', (req, res)=>{
-    console.log(`Hello Halrem!`);
-    //res.send('Hello Harlem!');
-    console.log(users);
+// router.get('/', (req, res)=>{
+//     console.log(`Hello Harlem!`);
+//     //res.send('Hello Harlem!');
+//     console.log(users);
     
-    res.send(users);
+//     res.send(users);
     
-});
+// });
 
-router.post('/', (req, res)=>{
-    console.log(`West-Side`);
-    //console.log(req.body);
-    const user = req.body//This is where my post is being stored. I can push my post into the array below
-    users.push(user);//This is to add posts/objects/users to my array.
+// router.post('/', (req, res)=>{
+//     console.log(`West-Side`);
+//     //console.log(req.body);
+//     const user = req.body//This is where my post is being stored. I can push my post into the array below
+//     users.push(user);//This is to add posts/objects/users to my array.
 
-    res.send(`Hey ${user.firstName}${user.lastName}! Where is the next fight for freedom?`);//testing post requests and variables from user
-});
+//     res.send(`Hey ${user.firstName}${user.lastName}! Where is the next fight for freedom?`);//testing post requests and variables from user
+// });
 
-router.get('/:id', (req, res)=>{// I want to access users
-    console.log(req.params);
-    const id = req.params;
-    res.send(`Your user is ` + id)//don't know why this is not working as expected
-})
+// router.get('/:id', (req, res)=>{// I want to access users
+//     console.log(req.params);
+//     const id = req.params;
+//     res.send(`Your user is ` + id)//don't know why this is not working as expected
+// })
 
-router.put('/:id', (req, res)=>{// I want to access users
-    res.send(`Your new user is ` + id)//don't know why this is not working as expected
-})
+// router.put('/:id', (req, res)=>{// I want to access users
+//     res.send(`Your new user is ` + id)//don't know why this is not working as expected
+// })
 
-router.delete('/:id', (req, res)=>{// I want to access users
-    res.send(`Delete user of id ` + id)//don't know why this is not working as expected
-})
+// router.delete('/:id', (req, res)=>{// I want to access users
+//     res.send(`Delete user of id ` + id)//don't know why this is not working as expected
+// })
 
 
-export default router;//exporting router for index.js
+// export default router;//exporting router for index.js
